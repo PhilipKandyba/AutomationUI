a046c96eae3d5b0a2641ac29da8ffc15a54381dc
PhilipKandyba <philip.kanduba@gmail.com>
2017-12-27 17:13:27 +0200

    Add Mongodb

--- tools/check_email.py
+++ tools/check_email.py
@@ -7,7 +7,7 @@ import time
 def check_email(sub):
     time.sleep(10)
     mail = imaplib.IMAP4_SSL('imap.gmail.com')
-    mail.login('philip.kanduba@gmail.com', 'filosof0955567051')
+    mail.login('***', '***')
     mail.select("inbox")
     for i in range(30):
         try:
